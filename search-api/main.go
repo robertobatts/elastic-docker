325e6117ce1d7fb77cc597d369ae1f1da828817c
Roberto Battaglia <battaroberto@gmail.com>
2019-09-23 22:10:17 +0200

    connecting go to elasticsearch

--- search-api/main.go
+++ search-api/main.go
@@ -1,10 +1,46 @@
 package main
 
 import (
+  "encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 	"time"
   "github.com/olivere/elastic"
+  "github.com/gin-gonic/gin"
 )
 
+const (
+  elasticIndexName = "skills"
+)
+
+type Document struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+  Level     string    `json:"level"`
+  Type      string    `json:"type"`
+}
+
+type DocumentRequest struct {
+	Name    string `json:"name"`
+  Level   string `json:"level"`
+  Type    string `json:"type"`
+}
+
+type DocumentResponse struct {
+	Name      string      `json:"name"`
+	CreatedAt time.Time   `json:"created_at"`
+  Level     string      `json:"level"`
+  Type      string      `json:"type"`
+}
+
+type SearchResponse struct {
+	Time      string             `json:"time"`
+	Hits      string             `json:"hits"`
+	Documents []DocumentResponse `json:"documents"`
+}
 
 var (
 	elasticClient *elastic.Client
@@ -24,5 +60,89 @@ func main() {
       break
     }
   }
-  
+  // Start HTTP server
+	r := gin.Default()
+	r.POST("/documents", createDocumentsEndpoint)
+	r.GET("/search", searchEndpoint)
+	if err = r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}  
+}
+
+func createDocumentsEndpoint(c *gin.Context) {
+	// Parse request
+	var docs []DocumentRequest
+	if err := c.BindJSON(&docs); err != nil {
+		errorResponse(c, http.StatusBadRequest, "Malformed request body")
+		return
+	}
+	// Insert documents in bulk
+	bulk := elasticClient.
+		Bulk().
+		Index(elasticIndexName)
+	for _, d := range docs {
+		doc := Document{
+			Name:       d.Name,
+			CreatedAt:  time.Now().UTC(),
+      Level:      d.Level,
+      Type:       d.Type
+		}
+		bulk.Add(elastic.NewBulkIndexRequest().Id(doc.ID).Doc(doc))
+	}
+	if _, err := bulk.Do(c.Request.Context()); err != nil {
+		log.Println(err)
+		errorResponse(c, http.StatusInternalServerError, "Failed to create documents")
+		return
+	}
+	c.Status(http.StatusOK)
+}
+
+func searchEndpoint(c *gin.Context) {
+	// Parse request
+	query := c.Query("query")
+	if query == "" {
+		errorResponse(c, http.StatusBadRequest, "Query not specified")
+		return
+	}
+	skip := 0
+	take := 10
+	if i, err := strconv.Atoi(c.Query("skip")); err == nil {
+		skip = i
+	}
+	if i, err := strconv.Atoi(c.Query("take")); err == nil {
+		take = i
+	}
+	// Perform search
+	esQuery := elastic.NewMultiMatchQuery(query, "Name", "Level").
+		Fuzziness("2").
+		MinimumShouldMatch("2")
+	result, err := elasticClient.Search().
+		Index(elasticIndexName).
+		Query(esQuery).
+		From(skip).Size(take).
+		Do(c.Request.Context())
+	if err != nil {
+		log.Println(err)
+		errorResponse(c, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+	res := SearchResponse{
+		Time: fmt.Sprintf("%d", result.TookInMillis),
+		Hits: fmt.Sprintf("%d", result.Hits.TotalHits),
+	}
+	// Transform search results before returning them
+	docs := make([]DocumentResponse, 0)
+	for _, hit := range result.Hits.Hits {
+		var doc DocumentResponse
+		json.Unmarshal(*hit.Source, &doc)
+		docs = append(docs, doc)
+	}
+	res.Documents = docs
+	c.JSON(http.StatusOK, res)
+}
+
+func errorResponse(c *gin.Context, code int, err string) {
+	c.JSON(code, gin.H{
+		"error": err,
+	})
 }
